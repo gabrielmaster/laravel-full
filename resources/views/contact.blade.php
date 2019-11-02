07cbb3a172a0d6aed2f668a132022d3a170491d6
gabrielmaster <hard85@gmail.com>
2019-11-01 20:39:52 -0400

    19. Cómo validar formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,8 +6,20 @@
 
 <h1>Contact</h1>
 
-    <form action="">
-        <input type="email">
+
+<form method="post" action="{{ route('contact') }}">
+
+        @csrf
+
+<input type="text" name="name" placeholder="Nombre..." value="{{ old('name') }}"> <br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
+        <input type="text" name="email" placeholder="Email..." value="{{ old('email') }}"><br>
+        {!! $errors->first('email', '<small>:message</small><br>') !!}
+        <input type="text" name="subject" placeholder="Asunto..." value="{{ old('subject') }}"><br>
+        {!! $errors->first('subject', '<small>:message</small><br>') !!}
+        <textarea name="content" placeholder="Mensaje ...">{{ old('content') }}</textarea><br>
+        {!! $errors->first('content', '<small>:message</small><br>') !!}
+        <button>Enviar</button>
     </form>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/contact', 'contact')->name('contact');
 Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
+Route::view('/contact', 'contact')->name('contact');
+
+Route::post('contact', "MessagesController@store");
