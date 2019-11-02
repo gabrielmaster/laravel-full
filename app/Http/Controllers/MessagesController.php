1f0cd90a9e7ae3271000d22c934e9d2f7c70d957
gabrielmaster <hard85@gmail.com>
2019-11-02 15:12:59 -0400

    21. Cómo enviar emails en Laravel

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
+
 class MessagesController extends Controller
 {
     public function store()
     {
-        request()->validate([
+        $msg = request()->validate([
             'name' => 'required',
             "email" => 'required|email',
             "subject" => 'required',
             "content" => 'required|min:3'
-        ], ['name.required' => 'Porfavor, escribe tu nombe']);
+        ], [
+            'name.required' => __('I need your name')
+        ]);
 
-        return "Datos validados";
+        Mail::to('hard85@gmail.com')->queue(new MessageReceived($msg));
+
+        return "Mensaje enviado";
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<h1>Contact</h1>
+<h1>{{ __('Contact') }}</h1>
 
 
 <form method="post" action="{{ route('contact') }}">
@@ -19,7 +19,7 @@
         {!! $errors->first('subject', '<small>:message</small><br>') !!}
         <textarea name="content" placeholder="Mensaje ...">{{ old('content') }}</textarea><br>
         {!! $errors->first('content', '<small>:message</small><br>') !!}
-        <button>Enviar</button>
+        <button>@lang('Send')</button>
     </form>
 
 @endsection
